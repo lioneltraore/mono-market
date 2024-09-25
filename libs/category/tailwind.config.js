ad84ae4c6a5e33b0972e590276de98a55b4fbe23
Lionel Traore <hello@lioneltraore.com>
2024-09-25 00:51:45 +0000

    add category library

---
+++ libs/category/tailwind.config.js
@@ -0,0 +1,17 @@
+const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
+const { join } = require('path');
+const sharedTailwindConfig = require('../../libs/tailwind-preset/tailwind.config');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  presets: [sharedTailwindConfig],
+  content: [
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+    '../../node_modules/preline/preline.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
