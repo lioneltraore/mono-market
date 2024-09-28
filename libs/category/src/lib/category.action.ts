82eb42a8405d3ddec538052c973ed63279d88a91
Lionel Traore <hello@lioneltraore.com>
2024-09-28 00:14:11 +0000

    add category actions

--- libs/category/src/lib/category.action.ts
+++ libs/category/src/lib/category.action.ts
@@ -1,3 +1,10 @@
-import { createAction } from "@ngrx/store";
+import { createAction } from '@ngrx/store';
 
 export const categories = createAction('[Category] Get categories');
+export const success = createAction(
+  '[Category] Get categories success',
+  (categories: string[]) => ({ categories })
+);
+export const failure = createAction("[Category] Get categories failure",
+  (error: string) => ({ error })
+);
